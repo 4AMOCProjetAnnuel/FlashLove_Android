698915407f686238fe354ed9bd1cba01bd2be58b
Fodé Guirassy <fode.guirassy@ekino.com>
2018-03-12 17:48:19 +0100

    slack notification

---
+++ app/src/main/java/projetannuel/bigteam/com/mvp/BasePresenter.kt
@@ -0,0 +1,16 @@
+package projetannuel.bigteam.com.mvp
+
+/**
+ * BasePresenter -
+ * @author guirassy
+ * @version $Id$
+ */
+interface BasePresenter {
+
+    fun resume()
+
+    fun pause()
+
+    fun stop()
+
+}
